1c1b2eda75f01d14e5bfcfdeddfdc762e94131e2
Kitewrld <kitew7@gmail.com>
2023-06-29 18:13:07 +0900

    first commit

---
+++ src/main/java/com/project/losobeatz/losobeatzApplication.java
@@ -0,0 +1,13 @@
+package com.project.losobeatz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class losobeatzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(losobeatzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/losobeatz/LosobeatzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.losobeatz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LosobeatzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
