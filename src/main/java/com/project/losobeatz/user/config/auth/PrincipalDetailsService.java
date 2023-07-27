0d2a55c4f24ac8b76507c813b70ad4e381ea36cf
Kitewrld <kitew7@gmail.com>
2023-07-27 18:44:50 +0900

    commit

---
+++ src/main/java/com/project/losobeatz/user/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,27 @@
+package com.project.losobeatz.user.config.auth;
+
+import com.project.losobeatz.user.model.User;
+import com.project.losobeatz.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+// 시큐리티 설정에서 loginProcessingUrl("/login);
+// /login 요청이 오면 자동으로 UserDetailsService 타입으로 loC되어 있는 loadUserByUsername 함수가 실행
+@Service
+public class PrincipalDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUsername(username);
+        if (userEntity != null) {
+            return new PrincipalDetails(userEntity);
+        }
+        return null;
+    }
+}
--- src/main/java/com/project/losobeatz/user/repository/UserRepository.java
+++ src/main/java/com/project/losobeatz/user/repository/UserRepository.java
@@ -1,9 +1,8 @@
 package com.project.losobeatz.user.repository;
 
-import com.project.losobeatz.user.entity.UserEntity;
+import com.project.losobeatz.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
-
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByUsername(String username);
 }
