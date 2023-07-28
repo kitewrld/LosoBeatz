11251df9cc197e4aa4822b76a99a10d13ab6f5cf
Kitewrld <kitew7@gmail.com>
2023-07-28 19:14:35 +0900

    commit

--- src/main/java/com/project/losobeatz/user/config/SecurityConfig.java
+++ src/main/java/com/project/losobeatz/user/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.project.losobeatz.user.config;
 
+import com.project.losobeatz.user.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록 됨.
 @EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize 어노테이션 활성화.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
 
     // 해당 메서드의 리턴되는 오브젝트를 Ioc로 등록.
     @Bean
@@ -31,6 +36,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/");
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm")
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);
     }
 }
--- src/main/java/com/project/losobeatz/user/model/User.java
+++ src/main/java/com/project/losobeatz/user/model/User.java
@@ -29,9 +29,9 @@ public class User {
     private String password;
     private String email;
     private String role; //ROLE_USER, ROLE_ADMIN
-    // OAuth를 위해 구성한 추가 필드 2개
-//    private String provider;
-//    private String providerId;
+//  OAuth를 위해 구성한 추가 필드 2개
+    private String provider;
+    private String providerId;
     @CreationTimestamp
     private Timestamp createDate;
 }
